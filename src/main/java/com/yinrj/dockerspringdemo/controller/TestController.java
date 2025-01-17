732ff71fdbbe207c461b45d950139a7da7b479b3
YassinYin <152459711+YassinYin@users.noreply.github.com>
2025-01-17 15:10:11 +0800

    增加配置文件

--- src/main/java/com/yinrj/dockerspringdemo/controller/TestController.java
+++ src/main/java/com/yinrj/dockerspringdemo/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.yinrj.dockerspringdemo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,10 +13,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    @Value("${util.config.test}")
+    private String content;
+
 
     @GetMapping("test")
     public String test(@RequestParam("a") String a) {
         return a;
+    }
+
+
+    @GetMapping("test1")
+    public String test1(@RequestParam("a") String a) {
+        return a + " " + content;
     }
 
 }
